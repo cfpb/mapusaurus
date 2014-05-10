93aa6fc8594f08cd51ccc27e34722ea783700e5e
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-05-10 20:20:11 +0000

    Add template directories

--- institutions/institutions/settings/settings.py
+++ institutions/institutions/settings/settings.py
@@ -72,6 +72,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
