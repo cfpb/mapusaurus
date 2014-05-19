2e80372653e252e5c1c1c8f96d40eabb086e6542
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-05-19 13:37:57 +0000

    Added Census tract support

---
+++ institutions/geo/models.py
@@ -0,0 +1,40 @@
+# This is an auto-generated Django model module created by ogrinspect.
+from django.contrib.gis.db import models
+
+class StateCensusTract(models.Model):
+    """ 
+        This model represents the shapefile for census tracts per state. This
+        model is auto-generated using the ogrinspect Django command. 
+    """
+
+    statefp = models.CharField(max_length=2)
+    countyfp = models.CharField(max_length=3)
+    tractce = models.CharField(max_length=6)
+    geoid = models.CharField(max_length=11)
+    name = models.CharField(max_length=7)
+    namelsad = models.CharField(max_length=20)
+    mtfcc = models.CharField(max_length=5)
+    funcstat = models.CharField(max_length=1)
+    aland = models.FloatField()
+    awater = models.FloatField()
+    intptlat = models.CharField(max_length=11)
+    intptlon = models.CharField(max_length=12)
+    geom = models.MultiPolygonField(srid=4269)
+    objects = models.GeoManager()
+
+# Auto-generated `LayerMapping` dictionary for CensusTract model
+censustract_mapping = {
+    'statefp' : 'STATEFP',
+    'countyfp' : 'COUNTYFP',
+    'tractce' : 'TRACTCE',
+    'geoid' : 'GEOID',
+    'name' : 'NAME',
+    'namelsad' : 'NAMELSAD',
+    'mtfcc' : 'MTFCC',
+    'funcstat' : 'FUNCSTAT',
+    'aland' : 'ALAND',
+    'awater' : 'AWATER',
+    'intptlat' : 'INTPTLAT',
+    'intptlon' : 'INTPTLON',
+    'geom' : 'MULTIPOLYGON',
+}
