b3f0b56d4fd57f5a0339316e4d051a4884b8e6b5
CM Lubinski <cm.lubinski@gmail.com>
2014-05-19 17:46:00 +0000

    Model for race in the census data

--- institutions/censusdata/models.py
+++ institutions/censusdata/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Census2010Race(models.Model):
+    geoid = models.ForeignKey('geo.StateCensusTract', to_field='geoid',
+                              unique=True, db_index=True, primary_key=True)
+
+    total_pop = models.IntegerField()
+    while_alone = models.IntegerField()
+    black_alone = models.IntegerField()
+    amind_alone = models.IntegerField()
+    asian_alone = models.IntegerField()
+    pacis_alone = models.IntegerField()
+    other_alone = models.IntegerField()
+    two_or_more = models.IntegerField()
