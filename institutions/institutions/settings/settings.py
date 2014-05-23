0cdcb69fec4ccd5290b692f664ba22a885cf0c3a
CM Lubinski <cm.lubinski@gmail.com>
2014-05-23 15:13:21 +0000

    Add middleware classes for caching

--- institutions/institutions/settings/settings.py
+++ institutions/institutions/settings/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 ROOT_URLCONF = 'institutions.urls'
