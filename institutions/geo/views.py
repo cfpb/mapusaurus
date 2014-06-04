e308103ba752192fc8e0d973406c409b45738270
CM Lubinski <cm.lubinski@gmail.com>
2014-06-04 20:44:34 +0000

    Serialize both geo views the same way

--- institutions/geo/views.py
+++ institutions/geo/views.py
@@ -30,11 +30,16 @@ def tracts(request):
 
     tracts = StateCensusTract.objects.filter(
         countyfp=county_fips, statefp=state_fips).order_by('geoid')
+    tracts = tracts.values_list('geojson')
     tracts = _paged(tracts, request)
 
-    return HttpResponse(
-        GeoJSONSerializer().serialize(tracts, use_natural_keys=True),
-        content_type='application/json')
+    # We already have the json strings per model pre-computed, so just place
+    # them inside a static response
+    response = '{"crs": {"type": "link", "properties": {"href": '
+    response += '"http://spatialreference.org/ref/epsg/4326/", "type": '
+    response += '"proj4"}}, "type": "FeatureCollection", "features": [%s]}'
+    response = response % ', '.join(t[0] for t in tracts)
+    return HttpResponse(response, content_type='application/json')
 
 
 def tracts_in_rect(request):
