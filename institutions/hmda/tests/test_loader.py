57001f7dd53980928883251f5b402d1d4262224a
CM Lubinski <cm.lubinski@gmail.com>
2014-06-04 20:44:51 +0000

    Test hmda import

---
+++ institutions/hmda/tests/test_loader.py
@@ -0,0 +1,24 @@
+import os
+
+from django.test import TestCase
+
+from hmda.management.commands.load_hmda import Command
+from hmda.models import HMDARecord
+
+
+class LoadHmdaTest(TestCase):
+    fixtures = ['dummy_tracts']
+
+    def test_handle(self):
+        command = Command()
+        command.handle(os.path.join("hmda", "tests", "mock_2014.csv"))
+
+        # The mock data file contains 10 records, 8 for known states
+        self.assertEqual(8, HMDARecord.objects.count())
+        lenders = set(r.lender for r in HMDARecord.objects.all())
+        self.assertEqual(1, len(lenders))
+        self.assertTrue(('5' + '0000000319') in lenders)
+        self.assertTrue(('5' + '0000000435') in lenders)
+        self.assertTrue(('3' + '0000001281') in lenders)
+
+        HMDARecord.objects.all().delete()
---
+++ institutions/hmda/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from hmda.models import HMDARecord
+
+
+class HMDARecordTest(TestCase):
+    fixtures = ['many_tracts']
+
+    def test_auto_fields(self):
+        record = HMDARecord(
+            as_of_year=2014, respondent_id='0123456789', agency_code='3',
+            loan_amount_000s=55, action_taken=1, state_code='11',
+            county_code='222', census_tract='33300')
+        record.save()
+        self.assertEqual(record.lender, '30123456789')
+        self.assertEqual(record.geoid_id, '1122233300')
+        record.delete()
+
+        record = HMDARecord(
+            as_of_year=2014, respondent_id='01-345-789', agency_code='2',
+            loan_amount_000s=55, action_taken=1, state_code='ST',
+            county_code='COU', census_tract='CENSU')
+        record.auto_fields()
+        self.assertEqual(record.lender, '201-345-789')
+        self.assertEqual(record.geoid_id, 'STCOUCENSU')
