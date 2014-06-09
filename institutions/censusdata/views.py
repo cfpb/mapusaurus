70dca2041ff65358fbeaca8f5c98aac3763c793e
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-06-09 20:36:03 +0000

    First cut at an API that lets you specify which fields to bin

--- institutions/censusdata/views.py
+++ institutions/censusdata/views.py
@@ -1,6 +1,8 @@
 import json
 
 from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+import numpy as np
 
 from .models import Census2010RaceStats
 
@@ -35,3 +37,63 @@ def race_summary(request):
         return HttpResponse(json.dumps(data), content_type='application/json')
     else:
         raise Http404
+
+
+def find_bin_indices(field):
+    bins = np.array(field['bins'])
+    values = np.array(field['values'])
+    inds = np.digitize(values, bins)
+    return inds
+
+
+def process_statistics(statreq):
+    state_fips = statreq['state_fips']
+    county_fips = statreq['county_fips']
+
+    if county_fips and state_fips:
+
+        tract_data = race_by_county(county_fips, state_fips)
+        model_fields = [f.name for f in Census2010RaceStats._meta.fields]
+
+        data = {'data': {}}
+        bins = {}
+        raw_fields = []
+
+        for field in statreq['fields']:
+            if field['name'] in model_fields:
+                if field['type'] == 'binned':
+                    bins[field['name']] = {'values': [], 'bins': field['bins']}
+                else:
+                    raw_fields.append(field['name'])
+
+        statsids = []
+        for stats in tract_data:
+            statsids.append(stats.geoid_id)
+
+            for field_name in bins:
+                bins[field_name]['values'].append(getattr(stats, field_name))
+
+        for field, vbin in bins.items():
+            vbin['bin_indices'] = dict(zip(statsids, find_bin_indices(vbin)))
+
+        for stats in tract_data:
+            sdata = {}
+            for field in bins:
+                field_bin = '%s_bin' % field
+                sdata[field_bin] = bins[field]['bin_indices'][stats.geoid_id]
+
+            for field in raw_fields:
+                sdata[field] = getattr(stats, field)
+
+            data['data'][stats.geoid_id] = sdata
+            data['fields'] = statreq
+        return data
+
+
+@csrf_exempt
+def statistics_retriever(request):
+    if request.is_ajax():
+        statistics_request = json.loads(request.body)
+        statistics = process_statistics(statistics_request)
+        return HttpResponse(
+            json.dumps(statistics), content_type='application/json')
