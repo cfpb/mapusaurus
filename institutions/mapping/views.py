82750e849f40c708dbe0289934c2096ef2f05a47
CM Lubinski <cm.lubinski@gmail.com>
2014-06-20 21:08:50 +0000

    First pass at showing lender info

--- institutions/mapping/views.py
+++ institutions/mapping/views.py
@@ -2,8 +2,22 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.template.loader import select_template
 
+from respondants.models import Institution
+
 
 def home(request):
-    context = RequestContext(request, {})
+    """Display the map. If lender info is present, provide it to the
+    template"""
+    lender = request.GET.get('lender', '')
+    context = {}
+    if lender and len(lender) > 1 and lender[0].isdigit():
+        query = Institution.objects.filter(agency_id=int(lender[0]))
+        query = query.filter(ffiec_id=lender[1:])
+        query = query.select_related('agency', 'zip_code')
+        lender = query.first()
+        if lender:
+            context['lender'] = lender
+
+    context = RequestContext(request, context)
     template = select_template(['custom-index.html', 'index.html'])
     return HttpResponse(template.render(context))
