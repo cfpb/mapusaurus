37f3ac1d31e180979c5c275d8e23282e32406aea
CM Lubinski <cm.lubinski@gmail.com>
2014-07-02 20:58:14 +0000

    Use geojson tiles for minority data

--- institutions/mapping/static/mapping/js/map.js
+++ institutions/mapping/static/mapping/js/map.js
@@ -1,5 +1,24 @@
 'use strict';
 
+L.TileLayer.HookableGeoJSON = L.TileLayer.GeoJSON.extend({
+    _tileLoaded: function(tile) {
+        var geoids = _.map(tile.datum.features, function(feature) {
+                return feature.properties.geoid;
+            }),
+            newTracts = _.filter(geoids, function(geoid) {
+                return !_.has(Mapusaurus.dataStore.tract, geoid);
+            });
+
+        //  "super"
+        L.TileLayer.GeoJSON.prototype._tileLoaded.apply(this, arguments);
+
+        if (newTracts.length > 0) {
+            Mapusaurus.updateDataWithoutGeos(newTracts);
+            Mapusaurus.fetchMissingStats(newTracts);
+        }
+    }
+});
+
 var Mapusaurus = {
     //  Leaflet map
     map: null,
@@ -7,6 +26,7 @@ var Mapusaurus = {
     layers: {tract: {minority: null}},
     //  Stores geo data, along with fields for layers
     dataStore: {tract: {}},
+    drawn: {},
     //  Stores stat data when the associated geos aren't loaded
     dataWithoutGeo: {tract: {minority: {}}},
     //  Keep track of which stateXcounties we've loaded
@@ -15,19 +35,24 @@ var Mapusaurus = {
     bubbleStyle: {fillColor: '#fff', fillOpacity: 0.9, weight: 2,
                   color: '#000'},
     //  fillColor will be assigned when rendering
-    tractStyle: {fillOpacity: 0.7, weight: 2, color: '#babbbd'},
+    tractStyle: {stroke: true, fillOpacity: 0.7, weight: 2, color: '#babbbd',
+                 fill: true},
+    //  used when loading census tracts
+    noStyle: {stroke: false, fill: false},
 
     initialize: function (map) {
-        var tiles = new L.TileLayer.GeoJSON('/shapes/tiles/{z}/{x}/{y}');
         map.setView([41.88, -87.63], 12);
         Mapusaurus.map = map;
         Mapusaurus.addKey(map);
-        map.addLayer(tiles);
-        Mapusaurus.layers.tract.minority = L.geoJson(
-            {type: 'FeatureCollection', features: []},
-            {onEachFeature: Mapusaurus.eachMinority,
-             style: Mapusaurus.minorityContinuousStyle}
-        );
+        Mapusaurus.layers.tract.minority = new L.TileLayer.HookableGeoJSON(
+            '/shapes/tiles/{z}/{x}/{y}', {}, {
+                onEachFeature: Mapusaurus.eachTract,
+                style: Mapusaurus.minorityContinousStyle,
+                // Don't redraw any tracts
+                filter: function(feature) {
+                    return !_.has(Mapusaurus.drawn, feature.properties.geoid);
+                }
+        });
         Mapusaurus.layers.tract.minority.addTo(map);
         if (Mapusaurus.urlParam('lender')) {
             Mapusaurus.layers.tract.loanVolume = L.layerGroup([]);
@@ -35,18 +60,15 @@ var Mapusaurus = {
             Mapusaurus.dataWithoutGeo.tract.loanVolume = {};
             Mapusaurus.statsLoaded.loanVolume = {};
         }
-        //  @todo: really, we only care about the part of the viewport which
-        //  is new
-        map.on('moveend', Mapusaurus.reloadGeo);
-        //  @todo: really, we only care on zoom-out + part which is new
-        map.on('zoomend', Mapusaurus.reloadGeo);
-        //  Connect style selector
+        //  Census tracts get cleared whenever zooming in/out (analogous to
+        //  other tile layers)
+        map.on('zoomstart', function() { Mapusaurus.drawn = {}; });
+
+        //  Selector to change bucket/continuous shading
         $('#style-selector').on('change', function() {
             Mapusaurus.layers.tract.minority.setStyle(
                 Mapusaurus[$('#style-selector').val()]);
         });
-        //  Kick it off
-        Mapusaurus.reloadGeo();
     },
 
     /* Indicates what the colors mean */
@@ -74,59 +96,46 @@ var Mapusaurus = {
         return params[field];
     },
 
-    /* Whenever the map is shifted/zoomed, reload geo data */
-    reloadGeo: function() {
-        var bounds = Mapusaurus.map.getBounds(),
-            xtraWidth = Math.abs(bounds.getEast() - bounds.getWest()) / 3,
-            xtraHeight = Math.abs(bounds.getNorth() - bounds.getSouth()) / 3,
-            //  NW-hemisphere centric
-            expandedN = bounds.getNorth() + xtraHeight,
-            expandedS = bounds.getSouth() - xtraHeight,
-            expandedW = bounds.getWest() - xtraWidth,
-            expandedE = bounds.getEast() + xtraWidth;
-
-        /* To be responsive, only load census tract data at zoom-level 10
-         * or above */
-        if (Mapusaurus.map.getZoom() >  10) {
-            Mapusaurus.loadTractData(
-                1, 
-                L.latLngBounds(L.latLng(expandedS, expandedW),
-                               L.latLng(expandedN, expandedE)));
+    /* Style/handling function for each census tract loaded from the geoJson
+     * tile layer. Adds interactions */
+    eachTract: function(feature, layer) {
+        var geoid = feature.properties.geoid;
+        Mapusaurus.drawn[geoid] = true;
+        if (!_.has(Mapusaurus.dataStore.tract, geoid)) {
+            Mapusaurus.dataStore.tract[geoid] = feature.properties;
         }
+        //  keep expected functionality with double clicking
+        layer.on('dblclick', function(ev) {
+            Mapusaurus.map.setZoomAround(ev.latlng,
+                                         Mapusaurus.map.getZoom() + 1);
+        });
+        layer.on('mouseover mousemove', function(e){
+            var marker = new L.Rrose({
+                offset: new L.Point(0, -10),
+                closeButton: false, 
+                autoPan: false
+            });
+            Mapusaurus.dataStore.tract[geoid].marker = marker;
+            marker.setContent(Mapusaurus.tractHoverText(geoid))
+                  .setLatLng(e.latlng)
+                  .openOn(Mapusaurus.map);
+        });
+        layer.on('mouseout', function(){
+            Mapusaurus.map.closePopup();
+        });
     },
 
-    /* Load census tract geo data (by pages) into dataStore. Bounds should
-     * remain consistent between pages of results */
-    loadTractData: function(page, bounds) {
-        //  This is north-western-hemisphere-centric for now
-        $.getJSON('/shapes/tracts-in/?minlat=' + bounds.getSouth().toString() +
-                  '&maxlat=' + bounds.getNorth().toString() +
-                  '&minlon=' + bounds.getWest().toString() +
-                  '&maxlon=' + bounds.getEast().toString() +
-                  '&page_num=' + page.toString(),
-                  function(data) {
-            var newTracts = [];   //  geos we've never seen
-
-            //  Collect new tracts, add them to dataStore
-            _.each(data.features, function(feature) {
-                var geoid = feature.properties.geoid;
-                if (!_.has(Mapusaurus.dataStore.tract, geoid)) {
-                    Mapusaurus.dataStore.tract[geoid] = feature;
-                    newTracts.push(geoid);
-                }
-            });
-
-            //  Check if there's anything new to draw, load stats data
-            if (newTracts.length > 0) {
-                Mapusaurus.updateDataWithoutGeos(newTracts);
-                Mapusaurus.fetchMissingStats(newTracts);
-            }
-
-            //  Continue with next page of geo results
-            if (data.features.length > 0) {
-                Mapusaurus.loadTractData(page + 1, bounds);
-            }
-        });
+    /* Depending on whether or not stats have been loaded, the hover text may
+     * be different */
+    tractHoverText: function(geoid) {
+        var tract = Mapusaurus.dataStore.tract[geoid];
+        if (_.has(tract, 'layer_minority')) {
+            return (
+                (1 - tract['layer_minority']['non_hisp_white_only_perc']) *
+                100).toFixed() + '% "Minority"';
+        } else {
+            return 'Loading...';
+        }
     },
 
     /*  We may have previously loaded stats data without the geos. Run through
@@ -140,7 +149,7 @@ var Mapusaurus = {
 
             //  For each shape
             _.each(newTracts, function(geoid) {
-                var geoProps = Mapusaurus.dataStore.tract[geoid].properties;
+                var geoProps = Mapusaurus.dataStore.tract[geoid];
                 //  Check if the data can now be drawn
                 if (_.has(undrawnData[layerName], geoid)) {
                     geoProps['layer_' + layerName] = 
@@ -161,14 +170,14 @@ var Mapusaurus = {
             //  We only care about unseen stat data
             var missingData = _.filter(newTracts, function(geoid) {
                 var geo = Mapusaurus.dataStore.tract[geoid],
-                    stateCounty = geo.properties.statefp +
-                                  geo.properties.countyfp;
+                    stateCounty = geo.statefp +
+                                  geo.countyfp;
                 return !Mapusaurus.statsLoaded[layerName][stateCounty];
             });
             //  convert to state + county strings
             missingData = _.map(missingData, function(geoid) {
                 var geo = Mapusaurus.dataStore.tract[geoid];
-                return geo.properties.statefp + geo.properties.countyfp;
+                return geo.statefp + geo.countyfp;
             });
             //  remove any duplicates; we end with what state/counties need to
             //  be retrieved
@@ -232,8 +241,8 @@ var Mapusaurus = {
                         Mapusaurus.dataWithoutGeo.tract[layerName][geoid] =
                             response[geoid];
                     //  Have the geo data, but haven't drawn the stats yet
-                    } else if (!geo.properties[llName]) {
-                        geo.properties[llName] = response[geoid];
+                    } else if (!geo[llName]) {
+                        geo[llName] = response[geoid];
                         toDraw[layerName].push(geoid);
                     }
                 });
@@ -253,18 +262,13 @@ var Mapusaurus = {
             });
             switch(layerName) {
                 case 'minority':
-                    // Filter out geos with no people; don't want to draw them
-                    geoData = _.filter(geoData, function(geo) {
-                        return geo.properties['layer_minority'][
-                            'total_pop'] > 0;
-                    });
-                    Mapusaurus.layers.tract.minority.addData(geoData);
-                    Mapusaurus.layers.tract.minority.bringToBack();
+                    Mapusaurus.layers.tract.minority.geojsonLayer.setStyle(
+                        Mapusaurus.minorityContinuousStyle);
                     break;
                 case 'loanVolume':
                     _.each(geoData, function(geo) {
                         Mapusaurus.layers.tract.loanVolume.addLayer(
-                            Mapusaurus.makeBubble(geo.properties)
+                            Mapusaurus.makeBubble(geo)
                         );
                     });
                     break;
@@ -310,34 +314,24 @@ var Mapusaurus = {
     },
     //  Shared function for minority styling; called by the two previous fns
     minorityStyle: function(feature, percentFn) {
-        var minorityPercent = 1 - feature.properties['layer_minority'][
+        var geoid = feature.properties.geoid,
+            tract = Mapusaurus.dataStore.tract[geoid];
+        //  If we haven't loaded the data yet or the tract has zero people,
+        //  don't draw it
+        if (!_.has(tract, 'layer_minority') ||
+            tract['layer_minority']['total_pop'] === 0) {
+            return Mapusaurus.noStyle;
+        } else {
+            var minorityPercent = 1 - tract['layer_minority'][
                                 'non_hisp_white_only_perc'],
-            bucket = Mapusaurus.toBucket(minorityPercent),
-            // convert given percentage to percents within bucket's bounds
-            bucketPercent = percentFn(minorityPercent, bucket);
-        return $.extend({}, Mapusaurus.tractStyle, {
-            fillColor: Mapusaurus.colorFromPercent(bucketPercent,
-                                                   bucket.colors)
-        });
-    },
-
-    /* Style/extras for each census tract in the minorities layer */
-    eachMinority: function(feature, layer) {
-      var nonMinorityPercent = feature.properties['layer_minority'][
-          'non_hisp_white_only_perc'];
-      //  keep expected functionality with double clicking
-      layer.on('dblclick', function(ev) {
-          Mapusaurus.map.setZoomAround(ev.latlng, Mapusaurus.map.getZoom() + 1);
-      });
-      layer.on('mouseover mousemove', function(e){
-          new L.Rrose({ offset: new L.Point(0,-10), closeButton: false, autoPan: false })
-              .setContent(((1 - nonMinorityPercent) * 100).toFixed() + '% "Minority"')
-              .setLatLng(e.latlng)
-              .openOn(Mapusaurus.map);
-      });
-      layer.on('mouseout', function(){
-          Mapusaurus.map.closePopup();
-      });
+                bucket = Mapusaurus.toBucket(minorityPercent),
+                // convert given percentage to percents within bucket's bounds
+                bucketPercent = percentFn(minorityPercent, bucket);
+            return $.extend({}, Mapusaurus.tractStyle, {
+                fillColor: Mapusaurus.colorFromPercent(bucketPercent,
+                                                       bucket.colors)
+            });
+        }
     },
 
     colorRanges: [
