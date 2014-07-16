1508156d905e67cb1b8acebec3ff0f2d95809208
danmurphy <daniel.murphy@cfpb.gov>
2014-07-16 10:09:32 -0400

    added humanize to project in order to format number (e.g. assets) in the template

--- institutions/institutions/settings/settings.py
+++ institutions/institutions/settings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'django.contrib.humanize',
     'localflavor',
     'south',
     'leaflet',
