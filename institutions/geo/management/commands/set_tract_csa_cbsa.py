17c5230fd008726f15fddb1b7bb3ea04f6d0782d
CM Lubinski <cm.lubinski@gmail.com>
2014-07-19 06:59:08 +0000

    Use named constants

--- institutions/geo/management/commands/set_tract_csa_cbsa.py
+++ institutions/geo/management/commands/set_tract_csa_cbsa.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
 
+from geo.models import Geo
+
 
 class Command(BaseCommand):
     help = """Set the CSA and CBSA of census tracts to that of their
@@ -11,17 +13,17 @@ class Command(BaseCommand):
         cursor.execute("""
             UPDATE geo_geo
             SET cbsa = (SELECT cbsa FROM geo_geo county
-                        WHERE county.geo_type = 2
+                        WHERE county.geo_type = %s
                         AND county.state = geo_geo.state
                         AND county.county = geo_geo.county)
-            WHERE geo_type = 3;
-        """)
+            WHERE geo_type = %s;
+        """, (Geo.COUNTY_TYPE, Geo.TRACT_TYPE))
         cursor.execute("""
             UPDATE geo_geo
             SET csa = (SELECT csa FROM geo_geo county
-                        WHERE county.geo_type = 2
+                        WHERE county.geo_type = %s
                         AND county.state = geo_geo.state
                         AND county.county = geo_geo.county)
-            WHERE geo_type = 3;
-        """)
+            WHERE geo_type = %s;
+        """, (Geo.COUNTY_TYPE, Geo.TRACT_TYPE))
         cursor.close()
