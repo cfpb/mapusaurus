07a80a5525ddaa6a195dc3ec5af5eaba04d7c917
CM Lubinski <cm.lubinski@gmail.com>
2014-07-20 04:24:21 +0000

    Create a link to HMDA explorer, based on metro divisions

--- institutions/mapping/views.py
+++ institutions/mapping/views.py
@@ -4,6 +4,10 @@ from geo.models import Geo
 from respondants.models import Institution
 
 
+DOWNLOAD_URL = "http://www.consumerfinance.gov/hmda/explore#!/as_of_year=2012"
+DOWNLOAD_URL += "&respondent_id=%s&agency_code=%s"
+
+
 def home(request):
     """Display the map. If lender info is present, provide it to the
     template"""
@@ -17,6 +21,8 @@ def home(request):
         lender = query.first()
         if lender:
             context['lender'] = lender
+    else:
+        lender = None
     if metro:
         query = Geo.objects.filter(geo_type=Geo.METRO_TYPE,
                                    geoid=metro)
@@ -24,4 +30,29 @@ def home(request):
         if metro:
             context['metro'] = metro
 
+    context['download_url'] = make_download_url(lender, metro)
+
     return render(request, 'index.html', context)
+
+
+def make_download_url(lender, metro):
+    """Create a link to CFPB's HMDA explorer, either linking to all of this
+    lender's records, or to just those relevant for an MSA. MSA's are broken
+    into divisions in that tool, so make sure the query uses the proper ids"""
+    if lender:
+        download_url = DOWNLOAD_URL % (lender.ffiec_id,
+                                       str(lender.agency_id))
+        if metro:
+            divisions = [div.metdiv for div in
+                         Geo.objects.filter(
+                             geo_type=Geo.METDIV_TYPE, cbsa=metro.geoid
+                         ).order_by('geoid')]
+            # convert into msamd-1, msamd-2, etc. key-value strings
+            divisions = ['msamd-' + str(idx + 1) + '=' + div
+                         for idx, div in enumerate(divisions)]
+            if divisions:
+                download_url += '&' + '&'.join(divisions)
+            else:   # no divisions, so just use the MSA
+                download_url += '&msamd-1=' + metro.geoid
+
+        return download_url
