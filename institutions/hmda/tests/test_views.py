c95944ded9541b00e4d44370834e76c7047fbda3
CM Lubinski <cm.lubinski@gmail.com>
2014-07-24 14:35:17 +0000

    Remove much unused code. Fix up tests. Combine subquery filter code

--- institutions/hmda/tests/test_views.py
+++ institutions/hmda/tests/test_views.py
@@ -46,26 +46,16 @@ class ViewsTest(TestCase):
     def test_volume_400(self):
         resp = self.client.get(reverse('hmda:volume'))
         self.assertEqual(400, resp.status_code)
-        resp = self.client.get(reverse('hmda:volume'),
-                               {'county_fips': '1'})
+        resp = self.client.get(reverse('hmda:volume'), {'state': '1'})
         self.assertEqual(400, resp.status_code)
-        resp = self.client.get(reverse('hmda:volume'),
-                               {'state_fips': '1'})
+        resp = self.client.get(reverse('hmda:volume'), {'county': '03111'})
         self.assertEqual(400, resp.status_code)
-        resp = self.client.get(reverse('hmda:volume'),
-                               {'county_fips': '1', 'state_fips': '3'})
-        self.assertEqual(400, resp.status_code)
-        resp = self.client.get(reverse('hmda:volume'),
-                               {'county_fips': '1', 'lender': '3'})
-        self.assertEqual(400, resp.status_code)
-        resp = self.client.get(reverse('hmda:volume'),
-                               {'lender': '1', 'state_fips': '3'})
+        resp = self.client.get(reverse('hmda:volume'), {'lender': '3'})
         self.assertEqual(400, resp.status_code)
 
     def test_volume(self):
         resp = self.client.get(reverse('hmda:volume'),
-                               {'state_fips': '11', 'county_fips': '222',
-                                'lender': '11111111111'})
+                               {'county': '11222', 'lender': '11111111111'})
         resp = json.loads(resp.content)
         self.assertEqual(len(resp), 2)
         self.assertTrue('1122233300' in resp)
@@ -74,3 +64,13 @@ class ViewsTest(TestCase):
         self.assertTrue('1122233400' in resp)
         self.assertEqual(resp['1122233400']['volume'], 1)
         self.assertEqual(resp['1122233400']['volume_per_100_households'], 0.1)
+
+    def test_volume_multiple(self):
+        resp = self.client.get(reverse('hmda:volume'),
+                               {'county': ['11222', '11223'],
+                                'lender': '11111111111'})
+        resp = json.loads(resp.content)
+        self.assertEqual(len(resp), 3)
+        self.assertTrue('1122233300' in resp)
+        self.assertTrue('1122233400' in resp)
+        self.assertTrue('1122333300' in resp)
--- institutions/hmda/views.py
+++ institutions/hmda/views.py
@@ -1,7 +1,7 @@
-from django.db.models import Count, Q
+from django.db.models import Count
 from django.http import HttpResponseBadRequest
 
-from batch.conversions import use_GET_in
+from batch.conversions import use_GET_in, state_county_filter
 from hmda.models import HMDARecord
 
 
@@ -14,28 +14,6 @@ def volume_per_100_households(volume, num_households):
         return 0
 
 
-def originations_by_county(counties, lender):
-    by_state = {}
-    for county in counties:
-        state, county = county[:2], county[2:]
-        by_state[state] = by_state.get(state, []) + [county]
-
-    query = None
-    for state, counties in by_state.iteritems():
-        subquery = Q(geoid__state=state, geoid__county__in=counties)
-        if query:
-            query = query | subquery
-        else:
-            query = subquery
-
-    # actions 7-8 are preapprovals to ignore
-    return HMDARecord.objects.filter(
-        lender=lender, action_taken__lte=6
-        ).filter(query).values(
-        'geoid', 'geoid__census2010households__total'
-        ).annotate(volume=Count('geoid'))
-
-
 def loan_originations(request_dict):
     """Get loan originations for a given lender, county combination. This
     ignores year for the moment."""
@@ -44,7 +22,12 @@ def loan_originations(request_dict):
     lender = request_dict.get('lender', [])
 
     if counties and all(len(c) == 5 for c in counties) and lender:
-        query = originations_by_county(counties, lender[0])
+        query = HMDARecord.objects.filter(
+            # actions 7-8 are preapprovals to ignore
+            lender=lender[0], action_taken__lte=6
+        ).filter(state_county_filter(counties)).values(
+            'geoid', 'geoid__census2010households__total'
+        ).annotate(volume=Count('geoid'))
         data = {}
         for row in query:
             data[row['geoid']] = {
