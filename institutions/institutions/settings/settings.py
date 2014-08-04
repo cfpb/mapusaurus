7e475f6569821e2572981da08a72ab435f7d7a9f
Theresa Summa <theresa.summa@cfpb.gov>
2014-08-04 14:11:33 -0300

    update settings to use local elasticsearch

--- institutions/institutions/settings/settings.py
+++ institutions/institutions/settings/settings.py
@@ -103,7 +103,9 @@ LEAFLET_CONFIG = {
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'mapusaurus',
     },
 }
 
