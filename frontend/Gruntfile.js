c6c64fccbb056cca587d131b0a607c126fbd70e6
danmurphy <daniel.murphy@cfpb.gov>
2014-08-27 14:47:06 -0400

    switch to cf-core for typography from cf-typography as suggested by scott to try to fix avenir next issues in ie10; fixed main map layout issue in ie10.

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
     concat: {
       main: {
         src: [
-          'frontend/bower_components/normalize.css/normalize.css',
+          //'frontend/bower_components/normalize.css/normalize.css',
           'frontend/bower_components/font-awesome/css/font-awesome.min.css'
         ],
         dest: 'frontend/dist/css/vendor.min.css',
