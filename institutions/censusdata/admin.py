9b4e8be057c099037199f0785f6baa6e4a582cd6
danmurphy <daniel.murphy@cfpb.gov>
2014-10-08 15:02:51 -0400

    added hmda and census data to the admin console

--- institutions/censusdata/admin.py
+++ institutions/censusdata/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import Census2010Race, Census2010HispanicOrigin, Census2010Sex, Census2010Age, Census2010RaceStats, Census2010Households
+
 
 # Register your models here.
+admin.site.register(Census2010Race)
+admin.site.register(Census2010HispanicOrigin)
+admin.site.register(Census2010Sex)
+admin.site.register(Census2010Age)
+admin.site.register(Census2010RaceStats)
+admin.site.register(Census2010Households)
