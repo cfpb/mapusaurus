3bc4ab6e6864a1ae24e39d8e79475130966f6f8f
danmurphy <daniel.murphy@cfpb.gov>
2014-10-21 15:32:03 -0400

    modified metro search to pass template parameter; modified mapbox template to make sure map renders properly inside #map div

--- institutions/mapping/urls.py
+++ institutions/mapping/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, url
-#from mapping.views import map
-
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'mapping.views.map', {'template': 'map'}, name='map'),
-    url(r'^mapbox$', 'mapping.views.map', {'template': 'mapbox'}, name='mapbox')
+    url(r'^$', 'mapping.views.map', name='map')
 )
--- institutions/mapping/views.py
+++ institutions/mapping/views.py
@@ -8,11 +8,12 @@ from hmda.management.commands.calculate_loan_stats import (calculate_median_loan
 from respondants.models import Institution
 
 
-def map(request, template):
+def map(request):
     """Display the map. If lender info is present, provide it to the
     template"""
     lender = request.GET.get('lender', '')
     metro = request.GET.get('metro')
+    template = request.GET.get('template', 'leaflet')
     context = {}
     if lender and len(lender) > 1 and lender[0].isdigit():
         query = Institution.objects.filter(agency_id=int(lender[0]))
