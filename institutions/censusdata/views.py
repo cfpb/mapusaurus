c690320fe5109d1f3691f6f16041438da98b0eeb
Dev Mehta <mehtadev17@gmail.com>
2014-10-24 19:24:52 +0000

    adding ability to get data by geoid instead of county

--- institutions/censusdata/views.py
+++ institutions/censusdata/views.py
@@ -7,8 +7,25 @@ from batch.utils import use_GET_in, state_county_filter
 def race_summary(request_dict):
     """Race summary statistics"""
     counties = request_dict.get('county', [])
+    geoid = request_dict.get('geoid', [])
+    if geoid:
+        query = Census2010RaceStats.objects.filter(geoid_id__in=geoid)
+        data = {}
+        for stats in query:
+            data[stats.geoid_id] = {
+                'total_pop': stats.total_pop,
+                'hispanic': stats.hispanic,
+                'non_hisp_white_only': stats.non_hisp_white_only,
+                'non_hisp_black_only': stats.non_hisp_black_only,
+                'non_hisp_asian_only': stats.non_hisp_asian_only,
+                'hispanic_perc': stats.hispanic_perc,
+                'non_hisp_white_only_perc': stats.non_hisp_white_only_perc,
+                'non_hisp_black_only_perc': stats.non_hisp_black_only_perc,
+                'non_hisp_asian_only_perc': stats.non_hisp_asian_only_perc
+            }
+        return data
 
-    if counties and all(len(county) == 5 for county in counties):
+    elif counties and all(len(county) == 5 for county in counties):
         query = Census2010RaceStats.objects.filter(
             state_county_filter(counties))
         data = {}
@@ -26,7 +43,7 @@ def race_summary(request_dict):
             }
         return data
     else:
-        return HttpResponseBadRequest("Missing county or county is not len 5")
+        return HttpResponseBadRequest("Missing geoid or county")
 
 
 def race_summary_http(request):
