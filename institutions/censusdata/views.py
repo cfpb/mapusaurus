e4566e4f0280fac5e2baf6088bf6c80cde2a6e42
Dev Mehta <mehtadev17@gmail.com>
2014-10-29 15:52:33 -0400

    enhancing endpoints

--- institutions/censusdata/views.py
+++ institutions/censusdata/views.py
@@ -5,18 +5,19 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from .models import Census2010RaceStats
 from geo.views import get_censustract_geoids
 
-def race_summary(request_dict, northEastLat, northEastLon, southWestLat, southWestLon):
+def race_summary(request):
     """Race summary statistics"""
-    geoid = get_censustract_geoids(request_dict, northEastLat, northEastLon, southWestLat, southWestLon)
-    if geoid:
-        query = Census2010RaceStats.objects.filter(geoid_id__in=geoid)
+    northEastLat, northEastLon, southWestLat, southWestLon = request.GET.get('neLat', []), request.GET.get('neLon', [    ]), request.GET.get('swLat', []), request.GET.get('swLon', [])
+    geoids = get_censustract_geoids(request, northEastLat, northEastLon, southWestLat, southWestLon)
+    if geoids:
+        query = Census2010RaceStats.objects.filter(geoid_id__in=geoids)
         return query
     else:
         return HttpResponseBadRequest("Missing geoid or county")
 
 
-def race_summary_as_json(request_dict, northEastLat, northEastLon, southWestLat, southWestLon):
-    records = race_summary(request_dict, northEastLat, northEastLon, southWestLat, southWestLon)
+def race_summary_as_json(request_dict):
+    records = race_summary(request_dict)
     data = {}
     for stats in records:
         data[stats.geoid_id] = {
@@ -34,5 +35,5 @@ def race_summary_as_json(request_dict, northEastLat, northEastLon, southWestLat,
 
 
 
-def race_summary_http(request, northEastLat, northEastLon, southWestLat, southWestLon):
-    return HttpResponse(json.dumps(race_summary_as_json(request, northEastLat, northEastLon, southWestLat, southWestLon)))
+def race_summary_http(request):
+    return HttpResponse(json.dumps(race_summary_as_json(request)))
--- institutions/hmda/urls.py
+++ institutions/hmda/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^volume/(?P<northEastLat>-?\d+\.\d{6})/(?P<northEastLon>-?\d+\.\d{6})/(?P<southWestLat>-?\d+\.\d{6})/(?P<southWestLon>-?\d+\.\d{6})/(?P<lender>-?\d+)/(?P<action_taken>-?\d+(,\d+)*)/$', 'hmda.views.loan_originations_http', name='volume'),
+    url(r'^volume/', 'hmda.views.loan_originations_http', name='volume'),
 )
