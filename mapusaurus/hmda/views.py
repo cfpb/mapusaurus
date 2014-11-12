cf739febb3668fdc928e1bfd53d3b958c3d54b28
Dev Mehta <mehtadev17@gmail.com>
2014-11-12 22:07:39 +0000

    initial trial of lender hierarchy

--- mapusaurus/hmda/views.py
+++ mapusaurus/hmda/views.py
@@ -5,16 +5,23 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from hmda.models import HMDARecord
 from geo.views import get_censustract_geoids 
 from rest_framework.renderers import JSONRenderer
-
+from respondents.lender_hierarchy_utils import get_related_lenders
 
 def loan_originations(request):
     """Get loan originations for a given lender, county combination. This
     ignores year for the moment."""
-    lender = request.GET.get('lender')
+    lender_id = request.GET.get('lender')
     action_taken_param = request.GET.get('action_taken')
-
+    lender_hierarchy = request.GET.get('super')
     geoids = get_censustract_geoids(request)
     action_taken = action_taken_param.split(',')
+    if lender_hierarchy:
+        lenders = get_related_lenders(lender_id)
+        if len(lenders) = 1 and lenders[0] = lender_id:
+            return HttpResponseRequest("No other lenders in organization")
+    else:
+        lenders = lender_id
+
     if geoids and lender and action_taken:
         query = HMDARecord.objects.filter(
             # actions 7-8 are preapprovals to ignore
