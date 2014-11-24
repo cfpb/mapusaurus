14829df193f90a2ea274d53ba2e2ce4361465cc2
Dev Mehta <mehtadev17@gmail.com>
2014-11-24 20:37:04 +0000

    updating mapping view to take template name as param

--- mapusaurus/mapping/urls.py
+++ mapusaurus/mapping/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'mapping.views.map', name='map'),
-    url(r'^print/', 'mapping.views.printmap', name='printmap')
+    url(r'^$', 'mapping.views.map', {'template':'map.html'}, name='map'),
+    url(r'^print/', 'mapping.views.map', {'template' :'print_map.html'}, name='printmap')
 )
