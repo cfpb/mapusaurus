9ddf5ae64c91038067d84261fe09dea8f5c76a6d
lucas.pfaff <lucas.pfaff@excella.com>
2014-12-01 15:29:27 -0500

    Now default order is asset desc

--- mapusaurus/respondents/views.py
+++ mapusaurus/respondents/views.py
@@ -87,7 +87,8 @@ def search_results(request):
     if current_sort in ('assets', '-assets', 'num_loans', '-num_loans'):
         query = query.order_by(current_sort)
     else:
-        current_sort = 'score'
+        query = query.order_by('-assets')
+        current_sort = '-assets'
 
     if lender_id:
         query = query.filter(lender_id=Exact(lender_id))
