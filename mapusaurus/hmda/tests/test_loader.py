02f4f137b3ac14193086e6b76b6642fd2f30254a
Dev Mehta <mehtadev17@gmail.com>
2014-12-23 22:34:05 +0000

    updating tests

--- mapusaurus/hmda/tests/test_loader.py
+++ mapusaurus/hmda/tests/test_loader.py
@@ -17,8 +17,8 @@ class LoadHmdaTest(TestCase):
 
         # The mock data file contains 10 records, 8 for known states
         self.assertEqual(8, HMDARecord.objects.count())
-        lenders = set(r.lender for r in HMDARecord.objects.all())
-        geos = set(r.geoid_id for r in HMDARecord.objects.all())
+        lenders = set(r.institution_id for r in HMDARecord.objects.all())
+        geos = set(r.geo_id for r in HMDARecord.objects.all())
         self.assertEqual(3, len(lenders))
         self.assertTrue(('5' + '0000000319') in lenders)
         self.assertTrue(('5' + '0000000435') in lenders)
@@ -38,7 +38,7 @@ class LoadHmdaTest(TestCase):
         command.stdout = Mock()
         command.handle(os.path.join("hmda", "tests", "mock_2014.csv"))
 
-        geos = set(r.geoid_id for r in HMDARecord.objects.all())
+        geos = set(r.geo_id for r in HMDARecord.objects.all())
         self.assertEqual(4, len(geos))
         # 1122233300 got replaced
         self.assertTrue('9988877766' in geos)
@@ -57,8 +57,8 @@ class LoadHmdaTest(TestCase):
 
         command.handle(main_csv_directory , "delete_file:false", "filterhmda" )
 
-        lenders = set(r.lender for r in HMDARecord.objects.all())
-        geos = set(r.geoid_id for r in HMDARecord.objects.all())
+        lenders = set(r.institution_id for r in HMDARecord.objects.all())
+        geos = set(r.geo_id for r in HMDARecord.objects.all())
 
         self.assertEqual(3, len(lenders))
         self.assertTrue(('5' + '0000000319') in lenders)
--- mapusaurus/hmda/tests/test_precalc.py
+++ mapusaurus/hmda/tests/test_precalc.py
@@ -57,25 +57,25 @@ class PrecalcTest(TestCase):
             'application_date_indicator':1}
         self.hmdas = []
         self.hmdas.append(HMDARecord.objects.create(
-            geoid=self.city_tract1, **hmda_params))
+            geo=self.city_tract1, institution=self.respondent, **hmda_params))
         for i in range(3):
             self.hmdas.append(HMDARecord.objects.create(
-                geoid=self.city_tract2, **hmda_params))
+                geo=self.city_tract2, institution=self.respondent, **hmda_params))
         for i in range(8):
             self.hmdas.append(HMDARecord.objects.create(
-                geoid=self.city_tract3, **hmda_params))
+                geo=self.city_tract3, institution=self.respondent, **hmda_params))
         for i in range(7):
             self.hmdas.append(HMDARecord.objects.create(
-                geoid=self.non_city_tract1, **hmda_params))
+                geo =self.non_city_tract1, institution=self.respondent, **hmda_params))
         for i in range(11):
             self.hmdas.append(HMDARecord.objects.create(
-                geoid=self.non_city_tract2, **hmda_params))
+                geo=self.non_city_tract2, institution=self.respondent, **hmda_params))
 
         hmda_params['respondent_id'] = 'other'
         # these should not affect the results, since they are another lender
         for i in range(3):
             self.hmdas.append(HMDARecord.objects.create(
-                geoid=self.city_tract2, **hmda_params))
+                geo=self.city_tract2, institution=self.respondent, **hmda_params))
 
     def tearDown(self):
         for hmda in self.hmdas:
@@ -89,21 +89,21 @@ class PrecalcTest(TestCase):
         self.metro.delete()
 
     def test_calculate_median_loans(self):
-        lender_id = str(self.respondent.agency_id) + self.respondent.respondent_id
+        lender_id = self.respondent.institution_id
         # 1 in tract 1, 3 in 2, 8 in 3, 0 in 4;             avg: 4, med: 3
         self.assertEqual(3, calculate_median_loans(lender_id, self.metro))
         # 1 in tract 1, 3 in 2, 8 in 3, 0 in 4; 7 in 5, 16 in 6; avg:6, med:7
         self.assertEqual(7, calculate_median_loans(lender_id, None))
 
     def test_saves_stats(self):
-        lender_id = str(self.respondent.agency_id) + self.respondent.respondent_id
+        lender_id = self.respondent.institution_id
         command = Command()
         command.stdout = Mock()
         command.handle()
 
         found = False
         for stats in LendingStats.objects.all():
-            if stats.geoid_id == '99999' and stats.lender == lender_id:
+            if stats.geo_id == '99999' and stats.institution_id == lender_id:
                 found = True
-                self.assertEqual(stats.median_per_tract, 3)
+                self.assertEqual(stats.lar_median, 3)
         self.assertTrue(found)
