fd5c6adb5618868e17a0e559c1a27e99632e363d
Dev Mehta <mehtadev17@gmail.com>
2015-01-14 15:44:20 +0000

    Create endpoint to export census data as csv

--- mapusaurus/censusdata/urls.py
+++ mapusaurus/censusdata/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^race_summary/', 'censusdata.views.race_summary_http', name='race_summary'),
+    url(r'^race_summary_csv/', 'censusdata.views.race_summary_csv', name='race_summary_csv'),
 )
--- mapusaurus/censusdata/views.py
+++ mapusaurus/censusdata/views.py
@@ -1,9 +1,13 @@
 import json
 
+from django.db.models import Count
 from django.http import HttpResponse, HttpResponseBadRequest
 
+from hmda.models import HMDARecord
 from .models import Census2010RaceStats
 from geo.views import get_censustract_geoids
+from geo.models import Geo
+from djqscsv import render_to_csv_response
 
 def race_summary(request):
     """Race summary statistics"""
@@ -34,3 +38,10 @@ def race_summary_as_json(request_dict):
 
 def race_summary_http(request):
     return HttpResponse(json.dumps(race_summary_as_json(request)))
+
+def race_summary_csv(request):
+    institution_id = request.GET.get('lender')
+    metro = request.GET.get('metro')
+    tracts_in_msa = Geo.objects.filter(geo_type=Geo.TRACT_TYPE, cbsa=metro).values_list('geoid', flat=True)
+    query = HMDARecord.objects.filter(institution_id=institution_id, geo_id__in=tracts_in_msa).values('geo_id', 'geo__census2010households__total', 'geo__census2010racestats__total_pop', 'geo__census2010racestats__hispanic_perc', 'geo__census2010racestats__non_hisp_white_only_perc', 'geo__census2010racestats__non_hisp_black_only_perc', 'geo__census2010racestats__non_hisp_asian_only_perc').annotate(volume=Count('geo_id'))
+    return render_to_csv_response(query)
