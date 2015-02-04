ba64ee3a13c13bc62991a7ee1f514cf7ce811b5e
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-02-04 16:03:54 -0500

    Adding in d3 map layer stuff; mocked out centroid drawing

--- frontend/src/js/map.js
+++ frontend/src/js/map.js
@@ -154,7 +154,7 @@ if (!window.console) console = {log: function() {}};
             createTractDataObj(); 
 
             // Redraw the circles using the created tract object AND the layer bubble type
-            redrawCircles(dataStore.tracts, layerInfo.type );
+            //redrawCircles(dataStore.tracts, layerInfo.type );
             
             // Unblock the user interface (remove gradient)
             $.unblockUI();
@@ -420,6 +420,51 @@ if (!window.console) console = {log: function() {}};
     /*
         END GET DATA SECTION
     */
+    /*
+     * Voronoi drawing
+     */
+    var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+    .addLayer(new L.TileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png"));
+
+    var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+        g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+    d3.json("us-states.json", function(collection) {
+      var transform = d3.geo.transform({point: projectPoint}),
+      path = d3.geo.path().projection(transform);
+
+      var feature = g.selectAll("path")
+        .data(collection.features)
+        .enter().append("path");
+
+      map.on("viewreset", reset);
+      reset();
+
+      // Reposition the SVG to cover the features.
+      function reset() {
+        var bounds = path.bounds(collection),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+
+        svg .attr("width", bottomRight[0] - topLeft[0])
+          .attr("height", bottomRight[1] - topLeft[1])
+          .style("left", topLeft[0] + "px")
+          .style("top", topLeft[1] + "px");
+
+        g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+        feature.attr("d", path);
+      }
+
+      // Use Leaflet to implement a D3 geometric transformation.
+      function projectPoint(x, y) {
+        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+        this.stream.point(point.x, point.y);
+      }
+    });
+    /*
+     * End Voronoi drawing
+     */
 
     /* 
         ---- DRAW CIRCLES AND MARKERS ----
@@ -772,7 +817,7 @@ if (!window.console) console = {log: function() {}};
         }
         
         addParam( 'category', $('#category-selector option:selected').val() );
-        updateCircles( layerType );
+        //updateCircles( layerType );
     }
 
     // Gets non-hash URL parameters
