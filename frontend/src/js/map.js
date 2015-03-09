30a77a11f42a5cdd80610da8f63c7e3f09d9300c
sephcoster <sephcoster@gmail.com>
2015-03-09 14:02:22 -0400

    added initial new navigation layout

--- frontend/src/js/map.js
+++ frontend/src/js/map.js
@@ -27,6 +27,48 @@ if (!window.console) console = {log: function() {}};
             setMapHeight();
         });
 
+        // When minority changes, redraw the circles with appropriate styles
+        $('#category-selector').on('change', function(e) {
+            var val = $('#category-selector').val();
+            layerUpdate(val);  
+        });
+
+        // Check to see if we have any parameters for category-selector
+        if( typeof loadParams.category !== 'undefined'){
+            $('#category-selector').val( loadParams.category.values );
+            layerUpdate( loadParams.category.values );
+        } else {
+            addParam( 'category', 'inv_non_hisp_white_only_perc' );
+            layerUpdate( 'inv_non_hisp_white_only_perc' );
+        }
+
+        if( typeof loadParams.category !== 'undefined'){
+            var cat = loadParams.category.values;
+            $('#' + cat ).addClass('active');
+            layerUpdate( cat );
+        }
+        
+        var mapDividerOptions = $('.map-divider-minor.option');
+        
+        mapDividerOptions.on('click', function(e){
+            mapDividerOptions.removeClass('active');
+            var selectedOption = $(this);
+            var optionVal = selectedOption.attr('id');
+            selectedOption.addClass('active');
+            addParam( 'category', optionVal );
+            layerUpdate( optionVal );
+            console.log("THIS CHANGE FOR MINOR OPTION: ", $(this));
+            console.log("E FOR MINOR OPTION CHANGE: ", e);
+        });
+
+        // Map divider on click change class and fire event for that change
+        // on('click', function(e){
+        //     removeClass('active') for all other elements
+        //     addClass('active') to this specific element
+        //     change the hash of the page.
+        //     layerUpdate();
+        // })
+
         // Check to see if we have any parameters for action-taken
         if( typeof loadParams.action !== 'undefined'){
             $('#action-taken-selector').val( loadParams.action.values );
