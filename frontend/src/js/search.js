28870200d38dfd6dc549c3a828f62e3c425ce30e
Chris Contolini <contolini@users.noreply.github.com>
2016-01-27 15:54:21 -0500

    Add year dropdown

--- frontend/src/js/search.js
+++ frontend/src/js/search.js
@@ -1,25 +1,35 @@
 'use strict';
 
 $(document).ready(function() {
-    var search = new Bloodhound({
-        datumTokenizer: function(d) {
-            return Bloodhound.tokenizers.whitespace(d.name);
-        },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: {
-            url: '/institutions/search/?auto=1&q=%QUERY',
-            filter: function(resp) { return resp.institutions;}
-        }
-    }),
-    
-    searchNameBox = $('#search_name');
-    search.initialize();
 
-    searchNameBox.typeahead({
-        highlight: true
-    }, {
-        displayKey: 'formatted_name',
-        source: search.ttAdapter()
+    function initSearch() {
+        var search = new Bloodhound({
+            datumTokenizer: function(d) {
+                return Bloodhound.tokenizers.whitespace(d.name);
+            },
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: '/institutions/search/?auto=1&q=%QUERY',
+                filter: function(resp) { return resp.institutions;}
+            }
+        }),
+        
+        searchNameBox = $('#search_name');
+        search.initialize();
+
+        searchNameBox.typeahead({
+            highlight: true
+        }, {
+            displayKey: 'formatted_name',
+            source: search.ttAdapter()
+        });
+    }
+
+    $('#search-year').on('change', function() {
+        var year = $(this).val();
+        $('form.year').addClass('hidden');
+        $('form.year.' + year).removeClass('hidden');
+        initSearch();
     });
 
 });
