b273cddc03a04f09fdb6431d1e024b5551e499e2
Chris Contolini <contolini@users.noreply.github.com>
2016-01-29 13:48:29 -0500

    Rough year selection implementation

--- frontend/src/js/search.js
+++ frontend/src/js/search.js
@@ -2,34 +2,33 @@
 
 $(document).ready(function() {
 
-    function initSearch() {
+    $('form.year').hide();
+
+    $('#search-year').on('change', function() {
+        var year = $(this).val();
+        $('form.year').hide();
+        $('form.year.' + year).show();
+
         var search = new Bloodhound({
+            initialize: false,
             datumTokenizer: function(d) {
                 return Bloodhound.tokenizers.whitespace(d.name);
             },
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             remote: {
-                url: '/institutions/search/?auto=1&q=%QUERY',
+                url: '/institutions/search/?auto=1&year=' + year + '&q=%QUERY',
                 filter: function(resp) { return resp.institutions;}
             }
-        }),
+        });
         
-        searchNameBox = $('#search_name');
         search.initialize();
-
-        searchNameBox.typeahead({
+        
+        $('form.year.' + year + ' .search_institution').typeahead({
             highlight: true
         }, {
             displayKey: 'formatted_name',
             source: search.ttAdapter()
         });
-    }
-
-    $('#search-year').on('change', function() {
-        var year = $(this).val();
-        $('form.year').addClass('hidden');
-        $('form.year.' + year).removeClass('hidden');
-        initSearch();
     });
 
 });
