703a05008a581a6282496fd451e29ccfbb190a56
Sam Leitner <samuel.leitner@cfpb.gov>
2016-02-05 14:22:47 -0500

    Make institution_id include year
    Institution id is now concatenation of year+agency_id+respondent_id (added
    year to make unique).

--- mapusaurus/respondents/management/commands/load_lender_hierarchy.py
+++ mapusaurus/respondents/management/commands/load_lender_hierarchy.py
@@ -2,12 +2,12 @@ import csv
 from django.core.management.base import BaseCommand
 from respondents.models import LenderHierarchy, Institution
 
-
 class Command(BaseCommand):
-    args = "<filename>"
+    args = "<year> <filename>"
 
     def handle(self, *args, **options):
-        hierarchy_filename = args[0]
+        year = args[0]
+        hierarchy_filename = args[1]
 
         with open(hierarchy_filename) as hierarchycsv:
             hierarchy_reader = csv.reader(hierarchycsv, delimiter=',')
@@ -16,6 +16,6 @@ class Command(BaseCommand):
                 record = LenderHierarchy(
                     organization_id=int(hierarchy_line[2]),
                 )
-                record.institution_id = hierarchy_line[0]+hierarchy_line[1].replace("'", "")
+                record.institution_id = year+hierarchy_line[0]+hierarchy_line[1].replace("'", "")
                 hierarchy.append(record)
             LenderHierarchy.objects.bulk_create(hierarchy)
