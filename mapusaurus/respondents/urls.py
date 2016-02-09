f80c7c922fcedbe2f16348e452815197344ab4fa
Sam Leitner <samuel.leitner@cfpb.gov>
2016-02-09 15:48:00 -0500

    Fix missing year haystack index etc until MSA search

--- mapusaurus/respondents/urls.py
+++ mapusaurus/respondents/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns(
     url(r'^search/$', 'respondents.views.search_results',
         name='search_results'),
     url(r'^$', 'respondents.views.search_home', name='search_home'),
-	url(r'^respondent/(?P<agency_id>[0-9])(?P<respondent>[0-9-]{10})',
+	url(r'^respondent/(?P<year>[0-9]{4})(?P<agency_id>[0-9])(?P<respondent>[0-9-]{10})',
 		'respondents.views.respondent', name='respondent_profile'),
     url(r'^branchLocations/(?P<northEastLat>-?\d+\.\d{6})/(?P<northEastLon>-?\d+\.\d{6})/(?P<southWestLat>-?\d+\.\d{6})/(?P<southWestLon>-?\d+\.\d{6})$',
         'respondents.views.branch_locations_as_json', name='branch_locations'),
