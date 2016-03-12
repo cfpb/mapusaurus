772fe84f75630c3f239c7c91bf102763cdfd03dd
Chris Contolini <contolini@users.noreply.github.com>
2016-03-11 19:46:07 -0500

    Add table CSV download view

--- mapusaurus/api/urls.py
+++ mapusaurus/api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     url(r'^hmda/', 'api.views.hmda', name='hmda'),
     url(r'^census/', 'api.views.census', name='census'),
     url(r'^tables/', 'api.views.tables', name='tables'),
+    url(r'^tables_csv/', 'api.views.tables_csv', name='tables_csv'),
     url(r'^msas/', 'api.views.msas', name='msas'),
     url(r'^tractCentroids/', 'api.views.tractCentroids', name='tractCentroids'),
     url(r'^branchLocations/', 'api.views.branch_locations', name='branchLocations'),
--- mapusaurus/api/views.py
+++ mapusaurus/api/views.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 from django.http import HttpResponse, HttpResponseNotFound
 from geo.views import geo_as_json, get_geos_by_bounds_and_type, get_censustract_geos
@@ -20,6 +21,28 @@ def tables(request):
     table_data = minority_aggregation_as_json(request)
     return HttpResponse(json.dumps(table_data), content_type='application/json')
 
+def tables_csv(request):
+    institution_id = request.GET.get('lender')
+    metro = request.GET.get('metro')
+    year = request.GET.get('year')
+
+    aggregation = minority_aggregation_as_json(request)
+    msa = aggregation['msa']
+    counties = aggregation['counties']
+    file_name = 'HMDA-Summary-Table_Year%s_Lender%s_MSA%s.csv' % (year, institution_id, metro)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=%s' % file_name
+
+    writer = csv.writer(response, csv.excel)
+    writer.writerow(['msa_or_county_id'] + counties.itervalues().next().keys())
+    # MSA has no county name so insert the word "MSA"
+    msa = msa.values()
+    msa.insert(1, 'MSA')
+    writer.writerow([institution_id] + msa)
+    for county in counties:
+        writer.writerow([county] + counties[county].values())
+    return response
+    
 def msas(request):
     """return a list of MSA ids visible by bounding coordinates"""
     try:
